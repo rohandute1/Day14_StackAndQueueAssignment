d3f6cf37805462397a54f37dacec37be7523ce9e
Rohan <rohandute1@gmail.com>
2023-06-18 15:20:44 +0530

    [Rohan] Successfully added stack

--- Day14_StackAndQueueAssignments/Program.cs
+++ Day14_StackAndQueueAssignments/Program.cs
@@ -11,6 +11,30 @@ namespace Day14_StackAndQueueAssignments
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue assignments");
+
+            Console.WriteLine("Choose any option from given below:-");
+            Console.WriteLine(" 1.Create Stack\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            
+            switch (option)
+            {
+                case 1:
+                    // Create a stack using a LinkedList
+                    LinkedList<int> stack = new LinkedList<int>();
+
+                    // Create an instance of the StackHelper class
+                    StackClass stackClass = new StackClass();
+
+                    // Push elements onto the stack
+                    stackClass.Push(stack, 70);
+                    stackClass.Push(stack, 30);
+                    stackClass.Push(stack, 56);
+
+                    // Print the stack
+                    Console.WriteLine("Stack elements:");
+                    stackClass.PrintStack(stack);
+                    break;
+            }
             Console.ReadLine();
         }
     }
---
+++ Day14_StackAndQueueAssignments/StackClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_StackAndQueueAssignments
+{
+    internal class StackClass
+    {
+        // Push method to add elements to the stack
+        public void Push(LinkedList<int> stack, int data)
+        {
+            stack.AddFirst(data);
+        }
+
+        // Helper method to print the stack elements
+        public void PrintStack(LinkedList<int> stack)
+        {
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
